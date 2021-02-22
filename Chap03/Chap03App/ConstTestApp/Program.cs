65f0291c374421e94728da999a1bf58c560612a2
hyojin-park24 <gygy332@naver.com>
2021-02-22 17:06:30 +0900

    3장 집가기 24분전

---
+++ Chap03/Chap03App/ConstTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConstTestApp
+{
+    class Program
+    {
+        enum Season // 열거형 ( 하나의 타입으로 표현 )
+        {
+            Spring,
+            Summer,
+            Fall,
+            Winter
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상수테스트");
+            const double PI = 3.141592;
+            Console.WriteLine($"원주율의 값는 {PI}");
+            // PI = 6.345;
+
+            Season mySeason = Season.Summer;
+            Console.WriteLine($"지금 계절은 핫 써머 아 핫핫 써머 일까요? {mySeason} 였습니다!");
+
+            int a = 0; //null 허용 안됌
+            Console.WriteLine($"a는 {a}");
+            int? b = null;
+            Console.WriteLine($"b는 {b}");
+
+        }
+    }
+}
---
+++ Chap03/Chap03App/VarTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 100.5f; //var는 지역 변수 선언 에서만 사용 가능, 밖에서 사용 부가능 float으로 밖에서 쓸 수 있음
+            Console.WriteLine($"정수값은 {i}입니다.");
+        }
+    }
+}
