04d79dd9794bbd84950baf0a8c76426ca95bb7c9
hyojin-park24 <gygy332@naver.com>
2021-02-22 15:15:50 +0900

    3장 중간 커밋

---
+++ Chap03/Chap03App/ArrayTestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("배열테스트");
+
+            int[] arr = new int[100000]; // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+            /*arr[0] = 1;
+           arr[1] = 2;
+           arr[2] = 3;*/
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"arr[{i}] 값은 {arr[i]}");
+            }
+
+        }
+    }
+}
