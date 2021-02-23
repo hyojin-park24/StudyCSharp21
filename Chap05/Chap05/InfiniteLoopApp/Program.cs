793dee21448ccfb5cfde300001bf8f1f8933d099
hyojin-park24 <gygy332@naver.com>
2021-02-23 15:16:38 +0900

    5장[중첩루프 / 점프문 / break문 / continue문]

---
+++ Chap05/Chap05/InfiniteLoopApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InfiniteLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+
+            long idx = 0;
+            for (; ; )//무한루프
+            {
+                Console.WriteLine($"idx = {idx++}");
+            }
+        }
+    }
+}
