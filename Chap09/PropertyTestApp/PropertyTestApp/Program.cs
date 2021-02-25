10de5ff1d0d6085cd7c3ea862d295b7443fdc7c5
hyojin-park24 <gygy332@naver.com>
2021-02-25 16:53:55 +0900

    9장 프로퍼티 (1)
    - Get/Set 이해 필요

---
+++ Chap09/PropertyTestApp/PropertyTestApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTestApp
+{
+    class Myclass
+    {
+        private int iValue; // 0~120℃ 값만 입력받는 멤버 변수
+        private double dZeta;
+        private float fPng;
+        private string strVal;
+        private int inCode;
+
+        public int IValue 
+        {
+            get {
+                return this.iValue;
+            }
+            set {
+                if (value < 0)
+                    this.iValue = 0;
+                else if (value > 120)
+                    this.iValue = 120;
+                else
+                    this.iValue = value;
+                
+            }
+        }
+
+        public Myclass(int iValue)
+        {
+            IValue = iValue;
+            //this.SetValue (iValue);
+        }
+
+       /* public int GetValue()
+        {
+            return this.iValue;
+        }
+
+        public void SetValue(int iValue)
+        {
+            if (value < 0)
+            {
+                this.iValue = 0;
+            }
+            else if (iValue > 120)
+            {
+                this.iValue = 120;
+            }
+            else
+            {
+                this.iValue = iValue;
+            }
+        }*/
+
+        public void PrintValue()
+        {
+            Console.WriteLine($"값은 {this.iValue}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass myclass = new Myclass(1200);
+            myclass.PrintValue();
+
+            //myclass.SetValue(108);
+            myclass.IValue = 1200;
+            myclass.PrintValue();
+            //Console.WriteLine($"현재 온도는 {myclass.GetValue()}℃ 입니다.");
+            Console.WriteLine($"현재 온도는 {myclass.IValue}℃ 입니다.");
+        }
+    }
+}
---
+++ Chap09/PropertyTestApp/PropertyTestApp2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PropertyTestApp2
+{
+    //생일정보 클래스
+    
+    class BirthdayInfo
+    {
+        private string name;
+        private DateTime birthday;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public DateTime Birthday
+        {
+            get { return this.birthday; }
+            set { this.birthday = value; }
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public DateTime GetBirthday()
+        {
+            return this.birthday;
+        }
+
+        public void SetBirthday(DateTime birthday)
+        {
+            this.birthday = birthday;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo info = new BirthdayInfo();
+            info.SetName("홍길동");
+            info.SetBirthday(new DateTime(1990,1,8));
+
+            Console.WriteLine($"이름 : {info.GetName()}");
+            Console.WriteLine($"생일 : {info.GetBirthday()}");
+
+            Console.WriteLine("프로퍼티 사용");
+
+            BirthdayInfo info2 = new BirthdayInfo();
+            info2.Name ="홍길순";
+            info2.Birthday= new DateTime(1992, 3, 26);
+
+            Console.WriteLine($"이름 : {info2.Name}");
+            Console.WriteLine($"생일 : {info2.Birthday}");
+        }
+    }
+}
