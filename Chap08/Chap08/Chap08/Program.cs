6ca08a9c0ea40bc71c377b3f04b56a879b624b19
hyojin-park24 <gygy332@naver.com>
2021-02-25 15:02:58 +0900

    8장 인터페이스 [ 제어의 역전 이해 필요 ]

---
+++ Chap08/Chap08/Chap08/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap08
+{
+    interface ILogger
+    {
+        void WriteLog(string msg);
+    }
+
+    class consoleLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"{DateTime.Now} log : {msg}");
+        }
+    }
+
+    class customLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"Log : {msg}\a");
+        }
+    }
+
+    class ClimateLogger 
+    {
+        private ILogger logger;
+        public ClimateLogger(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void start()
+        {
+            while (true)
+            {
+                Console.Write("온도를 입력 : ");
+                string temp = Console.ReadLine(); //
+                if (string.IsNullOrEmpty(temp)) break;
+
+                logger.WriteLog($"현재 온도 : {temp} ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new consoleLogger();
+            logger.WriteLog("로그출력!");
+
+            ClimateLogger clogger = new ClimateLogger(new customLogger());
+            clogger.start(); //IoC (제어의 역전(흐름) )
+        }
+    }
+}
---
+++ Chap08/Chap08/MultiInterfaceApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MultiInterfaceApp
+{
+    interface IRunnable // 자동차 종류
+    {
+        void Run();
+    }
+
+    interface IFlyable // 비행기 종류
+    {
+        void Fly();
+    }
+
+    class DroneCar : IRunnable, IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("날아가자!");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("달리자!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("드론카!");
+
+            DroneCar DreamCar = new DroneCar();
+            DreamCar.Run();
+            DreamCar.Fly();
+
+            Console.WriteLine("자동차로 변경 (interface)");
+            IRunnable car = DreamCar as IRunnable;
+            car.Run();
+            //car.Fly(); //erro
+
+            Console.WriteLine("비행기로 변경(interface)");
+            IFlyable plane = DreamCar;
+            plane.Fly();
+            //plane.Run();//erro
+
+        }
+    }
+}
