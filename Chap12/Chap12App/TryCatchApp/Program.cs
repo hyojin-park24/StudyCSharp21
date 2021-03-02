bea187c101b51649d40e24c1246a77f2659928f7
hyojin-park24 <gygy332@naver.com>
2021-03-02 15:31:14 +0900

    12장 [예외 처리] 프로그래밍을 '잘'만드는 것은 '오류'가 없는 것이다.

---
+++ Chap12/Chap12App/TryCatchApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TryCatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            try
+            {
+                
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"{i}번째 값 : {arr[i]}");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+
+            Console.WriteLine("나머지 일처리 계속...");
+            int x = 108, y = 0;
+            int result = 0;
+
+            try
+            {
+                result = x / y;
+                Console.WriteLine($"결과는 {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+            Console.WriteLine(" 또 나머지 일처리 계속...");
+        }
+    }
+}
