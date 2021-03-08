d726daf7af9fa1d121616a8a22e8cfdedcff9e8a
hyojin-park24 <gygy332@naver.com>
2021-03-08 15:45:46 +0900

    20장 [window form] 완료

---
+++ Chap20/Chap20App/FileCopyApp/FrmMain.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+using System;
+
+namespace FileCopyApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSource = new System.Windows.Forms.TextBox();
+            this.TxtTarget = new System.Windows.Forms.TextBox();
+            this.BtnSource = new System.Windows.Forms.Button();
+            this.BtnTarget = new System.Windows.Forms.Button();
+            this.BtnAsyncCopy = new System.Windows.Forms.Button();
+            this.BtnSyncCopy = new System.Windows.Forms.Button();
+            this.BtnCancle = new System.Windows.Forms.Button();
+            this.PrbCopy = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target : ";
+            // 
+            // TxtSource
+            // 
+            this.TxtSource.Location = new System.Drawing.Point(78, 19);
+            this.TxtSource.Name = "TxtSource";
+            this.TxtSource.Size = new System.Drawing.Size(406, 21);
+            this.TxtSource.TabIndex = 2;
+            this.TxtSource.TextChanged += new System.EventHandler(this.TxtSource_TextChanged);
+            // 
+            // TxtTarget
+            // 
+            this.TxtTarget.Location = new System.Drawing.Point(78, 43);
+            this.TxtTarget.Name = "TxtTarget";
+            this.TxtTarget.Size = new System.Drawing.Size(406, 21);
+            this.TxtTarget.TabIndex = 3;
+            // 
+            // BtnSource
+            // 
+            this.BtnSource.Location = new System.Drawing.Point(502, 13);
+            this.BtnSource.Name = "BtnSource";
+            this.BtnSource.Size = new System.Drawing.Size(28, 23);
+            this.BtnSource.TabIndex = 4;
+            this.BtnSource.Text = "...";
+            this.BtnSource.UseVisualStyleBackColor = true;
+            this.BtnSource.Click += new System.EventHandler(this.BtnSource_Click);
+            // 
+            // BtnTarget
+            // 
+            this.BtnTarget.Location = new System.Drawing.Point(502, 46);
+            this.BtnTarget.Name = "BtnTarget";
+            this.BtnTarget.Size = new System.Drawing.Size(28, 23);
+            this.BtnTarget.TabIndex = 5;
+            this.BtnTarget.Text = "...";
+            this.BtnTarget.UseVisualStyleBackColor = true;
+            this.BtnTarget.Click += new System.EventHandler(this.BtnTarget_Click);
+            // 
+            // BtnAsyncCopy
+            // 
+            this.BtnAsyncCopy.Location = new System.Drawing.Point(18, 91);
+            this.BtnAsyncCopy.Name = "BtnAsyncCopy";
+            this.BtnAsyncCopy.Size = new System.Drawing.Size(88, 34);
+            this.BtnAsyncCopy.TabIndex = 6;
+            this.BtnAsyncCopy.Text = "Async Copy";
+            this.BtnAsyncCopy.UseVisualStyleBackColor = true;
+            this.BtnAsyncCopy.Click += new System.EventHandler(this.BtnAsyncCopy_Click);
+            // 
+            // BtnSyncCopy
+            // 
+            this.BtnSyncCopy.Location = new System.Drawing.Point(232, 91);
+            this.BtnSyncCopy.Name = "BtnSyncCopy";
+            this.BtnSyncCopy.Size = new System.Drawing.Size(86, 34);
+            this.BtnSyncCopy.TabIndex = 7;
+            this.BtnSyncCopy.Text = "Sync Copy";
+            this.BtnSyncCopy.UseVisualStyleBackColor = true;
+            this.BtnSyncCopy.Click += new System.EventHandler(this.BtnSyncCopy_Click);
+            // 
+            // BtnCancle
+            // 
+            this.BtnCancle.Location = new System.Drawing.Point(441, 91);
+            this.BtnCancle.Name = "BtnCancle";
+            this.BtnCancle.Size = new System.Drawing.Size(89, 34);
+            this.BtnCancle.TabIndex = 8;
+            this.BtnCancle.Text = "Cancle";
+            this.BtnCancle.UseVisualStyleBackColor = true;
+            this.BtnCancle.Click += new System.EventHandler(this.BtnCancle_Click);
+            // 
+            // PrbCopy
+            // 
+            this.PrbCopy.Location = new System.Drawing.Point(17, 140);
+            this.PrbCopy.Name = "PrbCopy";
+            this.PrbCopy.Size = new System.Drawing.Size(513, 29);
+            this.PrbCopy.TabIndex = 9;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 188);
+            this.Controls.Add(this.PrbCopy);
+            this.Controls.Add(this.BtnCancle);
+            this.Controls.Add(this.BtnSyncCopy);
+            this.Controls.Add(this.BtnAsyncCopy);
+            this.Controls.Add(this.BtnTarget);
+            this.Controls.Add(this.BtnSource);
+            this.Controls.Add(this.TxtTarget);
+            this.Controls.Add(this.TxtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Async File Copy App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void BtnCancle_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TxtSource_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSource;
+        private System.Windows.Forms.TextBox TxtTarget;
+        private System.Windows.Forms.Button BtnSource;
+        private System.Windows.Forms.Button BtnTarget;
+        private System.Windows.Forms.Button BtnAsyncCopy;
+        private System.Windows.Forms.Button BtnSyncCopy;
+        private System.Windows.Forms.Button BtnCancle;
+        private System.Windows.Forms.ProgressBar PrbCopy;
+    }
+}
+
--- Chap20/Chap20App/UsingControlsApp/FrmMain.Designer.cs
+++ Chap20/Chap20App/UsingControlsApp/FrmMain.Designer.cs
@@ -39,9 +39,20 @@ namespace UsingControlsApp
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.PrbDisplay = new System.Windows.Forms.ProgressBar();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.BtnModal = new System.Windows.Forms.Button();
+            this.BtnModaless = new System.Windows.Forms.Button();
+            this.BtnMsgBox = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.TrvDummy = new System.Windows.Forms.TreeView();
+            this.LsvDummy = new System.Windows.Forms.ListView();
+            this.BtnAddRoot = new System.Windows.Forms.Button();
+            this.BtnAddChild = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -53,7 +64,7 @@ namespace UsingControlsApp
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(324, 104);
+            this.groupBox1.Size = new System.Drawing.Size(334, 104);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ComboBox,CheckBox,TextBox";
@@ -130,17 +141,110 @@ namespace UsingControlsApp
             this.groupBox2.Controls.Add(this.trackBar1);
             this.groupBox2.Location = new System.Drawing.Point(12, 122);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(321, 105);
+            this.groupBox2.Size = new System.Drawing.Size(331, 105);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "TrackBar_ProgressBar";
             this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.BtnMsgBox);
+            this.groupBox3.Controls.Add(this.BtnModaless);
+            this.groupBox3.Controls.Add(this.BtnModal);
+            this.groupBox3.Location = new System.Drawing.Point(13, 234);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(330, 85);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Modal,Modaless";
+            // 
+            // BtnModal
+            // 
+            this.BtnModal.Location = new System.Drawing.Point(16, 34);
+            this.BtnModal.Name = "BtnModal";
+            this.BtnModal.Size = new System.Drawing.Size(75, 24);
+            this.BtnModal.TabIndex = 0;
+            this.BtnModal.Text = "Modal";
+            this.BtnModal.UseVisualStyleBackColor = true;
+            this.BtnModal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnModaless
+            // 
+            this.BtnModaless.Location = new System.Drawing.Point(114, 34);
+            this.BtnModaless.Name = "BtnModaless";
+            this.BtnModaless.Size = new System.Drawing.Size(75, 23);
+            this.BtnModaless.TabIndex = 1;
+            this.BtnModaless.Text = "Modaless";
+            this.BtnModaless.UseVisualStyleBackColor = true;
+            this.BtnModaless.Click += new System.EventHandler(this.BtnModaless_Click);
+            // 
+            // BtnMsgBox
+            // 
+            this.BtnMsgBox.Location = new System.Drawing.Point(212, 34);
+            this.BtnMsgBox.Name = "BtnMsgBox";
+            this.BtnMsgBox.Size = new System.Drawing.Size(100, 23);
+            this.BtnMsgBox.TabIndex = 2;
+            this.BtnMsgBox.Text = "MessageBox";
+            this.BtnMsgBox.UseVisualStyleBackColor = true;
+            this.BtnMsgBox.Click += new System.EventHandler(this.BtnMsgBox_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.BtnAddChild);
+            this.groupBox4.Controls.Add(this.BtnAddRoot);
+            this.groupBox4.Controls.Add(this.LsvDummy);
+            this.groupBox4.Controls.Add(this.TrvDummy);
+            this.groupBox4.Location = new System.Drawing.Point(12, 326);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(331, 264);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "TreeView && ListView";
+            // 
+            // TrvDummy
+            // 
+            this.TrvDummy.Location = new System.Drawing.Point(17, 31);
+            this.TrvDummy.Name = "TrvDummy";
+            this.TrvDummy.Size = new System.Drawing.Size(140, 198);
+            this.TrvDummy.TabIndex = 0;
+            // 
+            // LsvDummy
+            // 
+            this.LsvDummy.HideSelection = false;
+            this.LsvDummy.Location = new System.Drawing.Point(174, 31);
+            this.LsvDummy.Name = "LsvDummy";
+            this.LsvDummy.Size = new System.Drawing.Size(140, 198);
+            this.LsvDummy.TabIndex = 1;
+            this.LsvDummy.UseCompatibleStateImageBehavior = false;
+            // 
+            // BtnAddRoot
+            // 
+            this.BtnAddRoot.Location = new System.Drawing.Point(14, 235);
+            this.BtnAddRoot.Name = "BtnAddRoot";
+            this.BtnAddRoot.Size = new System.Drawing.Size(65, 23);
+            this.BtnAddRoot.TabIndex = 2;
+            this.BtnAddRoot.Text = "루트추가";
+            this.BtnAddRoot.UseVisualStyleBackColor = true;
+            this.BtnAddRoot.Click += new System.EventHandler(this.BtnAddRoot_Click);
+            // 
+            // BtnAddChild
+            // 
+            this.BtnAddChild.Location = new System.Drawing.Point(85, 235);
+            this.BtnAddChild.Name = "BtnAddChild";
+            this.BtnAddChild.Size = new System.Drawing.Size(72, 23);
+            this.BtnAddChild.TabIndex = 3;
+            this.BtnAddChild.Text = "자식추가";
+            this.BtnAddChild.UseVisualStyleBackColor = true;
+            this.BtnAddChild.Click += new System.EventHandler(this.BtnAddChild_Click);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 331);
+            this.ClientSize = new System.Drawing.Size(357, 602);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -152,6 +256,8 @@ namespace UsingControlsApp
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -167,6 +273,15 @@ namespace UsingControlsApp
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.ProgressBar PrbDisplay;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BtnMsgBox;
+        private System.Windows.Forms.Button BtnModaless;
+        private System.Windows.Forms.Button BtnModal;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListView LsvDummy;
+        private System.Windows.Forms.TreeView TrvDummy;
+        private System.Windows.Forms.Button BtnAddChild;
+        private System.Windows.Forms.Button BtnAddRoot;
     }
 }
 
