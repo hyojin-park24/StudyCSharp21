e5171cf59e05b1108cedfa7a252f9c34b041ed14
hyojin-park24 <gygy332@naver.com>
2021-03-14 01:38:17 +0900

    C# 오버플로우 예제

---
+++ Chap03/Chap03App/OverFlowTestApp/OverflowTestApp/OverflowTestApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverflowTestApp
+{
+    public partial class Form1_Load : Form
+    {
+        public Form1_Load()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_Load(object sender, EventArgs e)
+        {
+                int flow = 2147483647;
+                textBox1.Text = Convert.ToString(flow + 1);
+        }
+    }
+}
