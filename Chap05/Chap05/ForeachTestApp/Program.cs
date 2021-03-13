dd9a4f35886e483039e68cbf71368ba8e0afb40c
hyojin-park24 <gygy332@naver.com>
2021-03-14 02:46:26 +0900

    C# 5장 전체 수정

--- Chap05/Chap05/ForeachTestApp/Program.cs
+++ Chap05/Chap05/ForeachTestApp/Program.cs
@@ -5,24 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ForeachTestApp
-{
-    class Program
+{  
+     class Program
     {
         static void Main(string[] args)
         {
-            int[] sources = { 1, 2, 3, 4, 5, 3, 6, 7, 8, 9, 10 };
+            int [] sources = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-           //랙\\f;
+            // for문
+            Console.WriteLine("For문");
             for (int i = 0; i < sources.Length; i++)
             {
-                Console.WriteLine($"sources[i] = {sources[i]}");
+                Console.WriteLine($"sources[{i}] = {sources[i]}");
             }
-            //foreach
+
+            // foreach
             Console.WriteLine("Foreach문");
             var idx = 0;
-            foreach(var item in sources)
+            foreach (var item in sources)
             {
-                Console.WriteLine($"sources = { item}");
+                Console.WriteLine($"sources[{idx++}] = {item}");
+            }
+
+            foreach (var item in sources)
+            {
+                if (item % 3 == 0)
+                {
+                    Console.WriteLine($"3의 배수 {item}");
+                }
+                else
+                    continue;
             }
         }
     }
